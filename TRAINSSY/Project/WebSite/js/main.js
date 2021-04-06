e99608427f03ec91423bf5ea50976ceaa1513c76
Vlad_PC <shvagron@gmail.com>
2021-04-06 03:02:13 +0300

    TRAINSSY: tabs done

--- TRAINSSY/Project/WebSite/js/main.js
+++ TRAINSSY/Project/WebSite/js/main.js
@@ -1,3 +1,14 @@
+// Dropdown menu of main header
+$('#dropDownMenu').on('mouseover', function () {
+  $("#openFullMenu").slideDown(400);
+});
+$('.horizontal_line').on('mouseover', function(e) {
+  if (!$('#dropDownMenu').is(e.target) && !$('#openFullMenu').is(e.target)) {
+    $("#openFullMenu").slideUp(400);
+  }
+});
+
+
 // SWIPER JS in offer construction
 let swiper1 = new Swiper('.slider1', {
     pagination: {
@@ -27,6 +38,25 @@ let swiper1 = new Swiper('.slider1', {
         }
     }
   });
+
+
+// TABS in arrivals section
+let jsTriggers = document.querySelectorAll('.arrivals-tab-trigger');
+
+jsTriggers.forEach(function(trigger) {
+   trigger.addEventListener('click', function() {
+      let id = this.getAttribute('data-tab'),
+          content = document.querySelector('.arrivals-tab-content[data-tab="'+id+'"]'),
+          activeTrigger = document.querySelector('.arrivals-tab-trigger.active'),
+          activeContent = document.querySelector('.arrivals-tab-content.active');
+      
+      activeTrigger.classList.remove('active');
+      trigger.classList.add('active');
+      
+      activeContent.classList.remove('active');
+      content.classList.add('active');
+   });
+});
 
 
 // SWIPER JS in reviews section
@@ -61,9 +91,11 @@ let swiper2 = new Swiper('.slider2', {
   }
 });
 
+
 // PARALLAX effect
 let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
+
 
 // Main anchor
 jQuery(document).ready(function() {
@@ -79,20 +111,4 @@ jQuery(document).ready(function() {
      e.preventDefault();
      $('html, body').animate({scrollTop:0}, '300');
    });
-});
-
-
-// Dropdown menu of main header
-$('#dropDownMenu').on('mouseover', function () {
-  $("#openFullMenu").slideDown(400);
-});
-$('.horizontal_line').on('mouseover', function(e) {
-  if (!$('#dropDownMenu').is(e.target) && !$('#openFullMenu').is(e.target)) {
-    $("#openFullMenu").slideUp(400);
-  }
-});
-$('#cart').on('mouseover', function(e) {
-  if (!$('#dropDownMenu').is(e.target) && !$('#openFullMenu').is(e.target)) {
-    $("#openFullMenu").slideUp(200);
-  }
 });
