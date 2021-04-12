102db0b4f5425330d97e2ad35309ab7073fa2735
Vlad_PC <shvagron@gmail.com>
2021-04-12 20:43:45 +0300

    TRAINSSY: sweets for LP performed

--- TRAINSSY/Project/WebSite/js/main.js
+++ TRAINSSY/Project/WebSite/js/main.js
@@ -7,6 +7,9 @@ let swiper5 = new Swiper('.slider5', {
   spaceBetween: 17.5,
   freeMode: true,
   breakpoints: {
+    280: {
+      slidesPerView: 1.4,
+    },
     320: {
       slidesPerView: 1.7,
     },
@@ -38,35 +41,41 @@ let swiper7 = new Swiper('.slider7', {
   spaceBetween: 17.5,
   freeMode: true,
   breakpoints: {
+    280: {
+      slidesPerView: 1,
+      freeMode: false,
+      spaceBetween: 30,
+    },
     320: {
       slidesPerView: 1.1,
+      freeMode: false,
+      spaceBetween: 17.5,
     },
     373: {
       slidesPerView: 1.3,
+      freeMode: true,
+      spaceBetween: 17.5,
     },
     400: {
       slidesPerView: 1.4,
+      freeMode: true,
+      spaceBetween: 17.5,
     },
     425: {
       slidesPerView: 1.5,
+      freeMode: true,
+      spaceBetween: 17.5,
     },
     473: {
       slidesPerView: 1.6,
+      freeMode: true,
+      spaceBetween: 17.5,
     },
     484: {
       slidesPerView: 1.7,
+      freeMode: true,
+      spaceBetween: 17.5,
     }
-  }
-});
-
-
-// Dropdown menu of main header
-$('#dropDownMenu').on('mouseover', function () {
-  $("#openFullMenu").slideDown(400);
-});
-$('.horizontal_line').on('mouseover', function(e) {
-  if (!$('#dropDownMenu').is(e.target) && !$('#openFullMenu').is(e.target)) {
-    $("#openFullMenu").slideUp(400);
   }
 });
 
@@ -120,7 +129,7 @@ jsTriggers.forEach(function(trigger) {
 });
 
 
-// SWIPER JS in news section (992 x 1200)
+// SWIPER JS in news section (1200 => 992)
 let swiper3 = new Swiper('.slider3', {
   simulateTouch: true,
   spaceBetween: 100,
@@ -141,6 +150,7 @@ let swiper3 = new Swiper('.slider3', {
   }
 });
 
+// SWIPER JS in small news section (768 => 0)
 let swiper6 = new Swiper('.slider6', {
   simulateTouch: true,
   touchAngle: 45,
@@ -149,29 +159,45 @@ let swiper6 = new Swiper('.slider6', {
   spaceBetween: 17.5,
   freeMode: true,
   breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+    },
     320: {
       slidesPerView: 1,
+      spaceBetween: 17.5,
     },
     342: {
       slidesPerView: 1.1,
+      spaceBetween: 17.5,
     },
     378: {
       slidesPerView: 1.2,
+      spaceBetween: 17.5,
     },
     408: {
       slidesPerView: 1.3,
+      spaceBetween: 17.5,
     },
     450: {
       slidesPerView: 1.4,
+      spaceBetween: 17.5,
     },
     502: {
       slidesPerView: 1.6,
+      spaceBetween: 17.5,
     },
     532: {
       slidesPerView: 1.7,
+      spaceBetween: 17.5,
     },
     576: {
       slidesPerView: 1.650,
+      spaceBetween: 17.5,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 1,
     },
   }
 });
@@ -194,6 +220,9 @@ let swiper2 = new Swiper('.slider2', {
   simulateTouch: true,
   spaceBetween: 200,
   breakpoints: {
+    280: {
+      slidesPerView: 1,
+    },
     320: {
       slidesPerView: 1,
     },
@@ -237,68 +266,6 @@ let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
 
 
-// Main anchor
-jQuery(document).ready(function() {
-  let btn = $('#btnUpDesktop');  
-  $(window).scroll(function() {     
-    if ($(window).scrollTop() > 300) {
-       btn.addClass('show');
-     } else {
-       btn.removeClass('show');
-     }
-   });
-   btn.on('click', function(e) {
-     e.preventDefault();
-     $('html, body').animate({scrollTop:0}, '300');
-   });
-});
-
-
-// Anchor for medium sizes
-jQuery(document).ready(function() {
-  let btn = $('#btnUp');  
-  $(window).scroll(function() {     
-    if ($(window).scrollTop() > 300) {
-       btn.addClass('show');
-     } else {
-       btn.removeClass('show');
-     }
-   });
-   btn.on('click', function(e) {
-     e.preventDefault();
-     $('html, body').animate({scrollTop:0}, '300');
-   });
-});
-
-// Mobile anchor
-jQuery(document).ready(function() {
-  let btn = $('#btnUpMobile');  
-  $(window).scroll(function() {     
-    if ($(window).scrollTop() > 300) {
-       btn.addClass('show');
-     } else {
-       btn.removeClass('show');
-     }
-   });
-   btn.on('click', function(e) {
-     e.preventDefault();
-     $('html, body').animate({scrollTop:0}, '300');
-   });
-});
-
-
-// Mobile menu
-$('.hamburger_menu').on('click', function() {
-  $('.menu_mobile').toggleClass('menu_mobile_active');
-  $('.content').toggleClass('content_active');
-});
-
-$('.navbar_mobile_close').on('click', function() {
-  $('.menu_mobile').toggleClass('menu_mobile_active');
-  $('.content').toggleClass('content_active');
-});
-
-
 // SWIPER JS in arrivals
 let swiper4 = new Swiper('.slider4', {
   simulateTouch: true,
@@ -316,21 +283,34 @@ let swiper4 = new Swiper('.slider4', {
     dragSize: 50,
   },
   breakpoints: {
+    280: {
+      slidesPerView: 1,
+      freeMode: false,
+    },
     320: {
-      slidesPerView: 1.6,
+      slidesPerView: 1.4,
+      freeMode: false,
+      spaceBetween: 100,
     },
     375: {
       slidesPerView: 1.6,
+      freeMode: true,
+      spaceBetween: 100,
     },
     425: {
       slidesPerView: 2,
+      freeMode: true,
+      spaceBetween: 150,
     },
     576: {
       slidesPerView: 2.1,
+      freeMode: true,
+      spaceBetween: 150,
     },
     768: {
       slidesPerView: 2.8,
+      freeMode: true,
+      spaceBetween: 150,
     },
   }
 });
-
