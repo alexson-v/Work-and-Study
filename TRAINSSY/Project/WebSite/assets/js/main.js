b6d472e9fb6f6b370935cdde1778bd7eb4e209d1
VladMac <vladyslavoleksiienko@gmail.com>
2021-08-11 06:49:56 +0300

    trainssy: cosmetic changes made

--- TRAINSSY/Project/WebSite/assets/js/main.js
+++ TRAINSSY/Project/WebSite/assets/js/main.js
@@ -1,316 +1,65 @@
-// Slider tab menu in arrivals
-let swiper5 = new Swiper('.slider5', {
-  simulateTouch: true,
-  touchAngle: 45,
-  draggable: true,
-  slidesPerView: 1.2,
-  spaceBetween: 17.5,
-  freeMode: true,
-  breakpoints: {
-    280: {
-      slidesPerView: 1.4,
-    },
-    320: {
-      slidesPerView: 1.7,
-    },
-    373: {
-      slidesPerView: 2,
-    },
-    400: {
-      slidesPerView: 2.2,
-    },
-    425: {
-      slidesPerView: 2.2,
-    },
-    473: {
-      slidesPerView: 2.6,
-    },
-    576: {
-      slidesPerView: 1.2,
-    },
-  }
-});
+document.addEventListener('DOMContentLoaded', () => {
 
-
-// Slider menu in special offers ( < 768)
-let swiper7 = new Swiper('.slider7', {
-  simulateTouch: true,
-  touchAngle: 45,
-  draggable: true,
-  slidesPerView: 1.2,
-  spaceBetween: 17.5,
-  freeMode: true,
-  breakpoints: {
-    280: {
-      slidesPerView: 1,
-      freeMode: false,
-      spaceBetween: 30,
-    },
-    320: {
-      slidesPerView: 1.1,
-      freeMode: false,
-      spaceBetween: 17.5,
-    },
-    373: {
-      slidesPerView: 1.3,
-      freeMode: true,
-      spaceBetween: 17.5,
-    },
-    400: {
-      slidesPerView: 1.4,
-      freeMode: true,
-      spaceBetween: 17.5,
-    },
-    425: {
-      slidesPerView: 1.5,
-      freeMode: true,
-      spaceBetween: 17.5,
-    },
-    473: {
-      slidesPerView: 1.6,
-      freeMode: true,
-      spaceBetween: 17.5,
-    },
-    484: {
-      slidesPerView: 1.7,
-      freeMode: true,
-      spaceBetween: 17.5,
-    }
-  }
-});
-
-
-// SWIPER JS in offer construction
-let swiper1 = new Swiper('.slider1', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    simulateTouch: true,
-    touchAngle: 45,
-    slidesPerView: 1,
-    draggable: true,
-    loop: true,
-    autoplay: {
-        delay: 5000,
-    }
+  // Табы (десктопная версия) секции "Новые поступления"
+  let jsTriggers = document.querySelectorAll('.arrivals-tab-trigger');
+  jsTriggers.forEach(function(trigger) {
+    trigger.addEventListener('click', function() {
+        let id = this.getAttribute('data-tab'),
+            content = document.querySelector('.arrivals-tab-content[data-tab="'+id+'"]'),
+            activeTrigger = document.querySelector('.arrivals-tab-trigger.active'),
+            activeContent = document.querySelector('.arrivals-tab-content.active');
+        activeTrigger.classList.remove('active');
+        trigger.classList.add('active');
+        activeContent.classList.remove('active');
+        content.classList.add('active');
+    });
   });
 
+  // Табы слайдеров (мобильная версия) секции "Новые поступления"
+  let jsTriggersSlider = document.querySelectorAll('.arrivals-tab-trigger');
+  jsTriggers.forEach(function(trigger) {
+    trigger.addEventListener('click', function() {
+        let id = this.getAttribute('data-tab'),
+            contentSlider = document.querySelector('.slider_arrivals-cards[data-tab="'+id+'"]'),
+            activeTrigger = document.querySelector('.arrivals-tab-trigger.active'),
+            activeContentSlider = document.querySelector('.slider_arrivals-cards.active');
+        activeTrigger.classList.remove('active');
+        trigger.classList.add('active');
+        activeContentSlider.classList.remove('active');
+        contentSlider.classList.add('active');
+    });
+  });
 
-// TABS in arrivals section
-let jsTriggers = document.querySelectorAll('.arrivals-tab-trigger');
-jsTriggers.forEach(function(trigger) {
-   trigger.addEventListener('click', function() {
-      let id = this.getAttribute('data-tab'),
-          content = document.querySelector('.arrivals-tab-content[data-tab="'+id+'"]'),
-          activeTrigger = document.querySelector('.arrivals-tab-trigger.active'),
-          activeContent = document.querySelector('.arrivals-tab-content.active');
-      activeTrigger.classList.remove('active');
-      trigger.classList.add('active');
-      activeContent.classList.remove('active');
-      content.classList.add('active');
-   });
+  // Эффект "parallax" в анимированном изображении
+  let scene = document.getElementById('scene');
+  let parallaxInstance = new Parallax(scene);
+
 });
 
 
-// TABS in arrivals section (SLIDERS)
-let jsTriggersSlider = document.querySelectorAll('.arrivals-tab-trigger');
-jsTriggers.forEach(function(trigger) {
-   trigger.addEventListener('click', function() {
-      let id = this.getAttribute('data-tab'),
-          contentSlider = document.querySelector('.slider4[data-tab="'+id+'"]'),
-          activeTrigger = document.querySelector('.arrivals-tab-trigger.active'),
-          activeContentSlider = document.querySelector('.slider4.active');
-      activeTrigger.classList.remove('active');
-      trigger.classList.add('active');
-      activeContentSlider.classList.remove('active');
-      contentSlider.classList.add('active');
-   });
-});
+// Главная страница -> (320px - 18.000px) -> Офферная конструкция
 
-
-// SWIPER JS in news section (1200 => 992)
-let swiper3 = new Swiper('.slider3', {
-  simulateTouch: true,
-  spaceBetween: 100,
-  touchAngle: 45,
-  slidesPerView: 1.4,
-  autoplay: {
-    delay: 10000,
-  },
-  loop: true,
-  draggable: true,
-  breakpoints: {
-      768: {
-        slidesPerView: 1.3,
+  const swiperOfferImg = new Swiper('.slider-offer-img', {
+      pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
       },
-      992: {
-        slidesPerView: 1.7,
+      slidesPerView: 1,
+      loop: true,
+      speed: 2000,
+      parallax: true,
+      autoplay: {
+      delay: 3000
       }
-  }
-});
+  });
+  
+  const swiperOfferText = new Swiper('.slider-offer-text', {
+      loop: true,
+      speed: 2000,
+      simulateTouch: true,
+      touchAngle: 45,
+      draggable: true
+  });
 
-// SWIPER JS in small news section (768 => 0)
-let swiper6 = new Swiper('.slider6', {
-  simulateTouch: true,
-  touchAngle: 45,
-  draggable: true,
-  slidesPerView: 1.6,
-  spaceBetween: 17.5,
-  freeMode: true,
-  breakpoints: {
-    280: {
-      slidesPerView: 1,
-      spaceBetween: 60,
-    },
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 17.5,
-    },
-    342: {
-      slidesPerView: 1.1,
-      spaceBetween: 17.5,
-    },
-    378: {
-      slidesPerView: 1.2,
-      spaceBetween: 17.5,
-    },
-    408: {
-      slidesPerView: 1.3,
-      spaceBetween: 17.5,
-    },
-    450: {
-      slidesPerView: 1.4,
-      spaceBetween: 17.5,
-    },
-    502: {
-      slidesPerView: 1.6,
-      spaceBetween: 17.5,
-    },
-    532: {
-      slidesPerView: 1.7,
-      spaceBetween: 17.5,
-    },
-    576: {
-      slidesPerView: 1.650,
-      spaceBetween: 17.5,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 1,
-    },
-  }
-});
-
-
-// SWIPER JS in reviews section
-let swiper2 = new Swiper('.slider2', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    clickable: true,
-  },
-  centeredSlides: true,
-  slidesPerView: 3.5,
-  simulateTouch: true,
-  spaceBetween: 200,
-  breakpoints: {
-    280: {
-      slidesPerView: 1,
-    },
-    320: {
-      slidesPerView: 1,
-    },
-    342: {
-      slidesPerView: 1,
-    },
-    378: {
-      slidesPerView: 1,
-    },
-    408: {
-      slidesPerView: 1.5,
-    },
-    450: {
-      slidesPerView: 1.6,
-    },
-    502: {
-      slidesPerView: 1.7,
-    },
-    532: {
-      slidesPerView: 1.8,
-    },  
-    576: {
-      slidesPerView: 1.9,
-      navigation: false,
-    },
-    768: {
-      slidesPerView: 2.5,
-    },
-    992: {
-      slidesPerView: 3.2,
-    },
-    1200: {
-      slidesPerView: 3.9,
-    }
-  }
-});
-
-
-// PARALLAX effect
-let scene = document.getElementById('scene');
-let parallaxInstance = new Parallax(scene);
-
-
-// SWIPER JS in arrivals
-let swiper4 = new Swiper('.slider4', {
-  simulateTouch: true,
-  touchAngle: 45,
-  draggable: true,
-  slidesPerView: 3,
-  loop: true,
-  spaceBetween: 150,
-  freeMode: true,
-  observer: true,
-  observeParents: true,
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-    dragSize: 50,
-  },
-  breakpoints: {
-    280: {
-      slidesPerView: 1,
-      freeMode: false,
-    },
-    320: {
-      slidesPerView: 1.4,
-      freeMode: false,
-      spaceBetween: 100,
-    },
-    375: {
-      slidesPerView: 1.6,
-      freeMode: true,
-      spaceBetween: 100,
-    },
-    425: {
-      slidesPerView: 2,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-    576: {
-      slidesPerView: 2.1,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-    768: {
-      slidesPerView: 2.8,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-  }
-});
+  swiperOfferImg.controller.control = swiperOfferText;
+  swiperOfferText.controller.control = swiperOfferImg;
--- TRAINSSY/Project/WebSite/assets/js/product.js
+++ TRAINSSY/Project/WebSite/assets/js/product.js
@@ -1,118 +1,119 @@
-// Табы меню товара
-const productTabTriggers = document.querySelectorAll(".product-tab__trigger"),
-      productTabContents = document.querySelectorAll(".main-block__product-tab"),
-      productStickyCard = document.querySelector('.main-block__product__sticky-card');
+document.addEventListener('DOMContentLoaded', () => {
 
-for (let i = 0; i < productTabTriggers.length; i++) {
+  // Табы меню товара
+  const productTabTriggers = document.querySelectorAll(".product-tab__trigger"),
+        productTabContents = document.querySelectorAll(".main-block__product-tab"),
+        productStickyCard = document.querySelector('.main-block__product__sticky-card');
 
-    productTabTriggers[i].addEventListener("click", function(e){
-    e.preventDefault();
-    const activeTabAttr = e.target.getAttribute("data-product-tab");
+  for (let i = 0; i < productTabTriggers.length; i++) {
 
-    for (let j = 0; j < productTabTriggers.length; j++) {
-      let contentAttr = productTabContents[j].getAttribute("data-product-content");
+      productTabTriggers[i].addEventListener("click", function(e){
+      
+      const activeTabAttr = e.target.getAttribute("data-product-tab");
 
-      if (activeTabAttr === contentAttr) {
-        productTabTriggers[j].classList.add("active");
-        productTabContents[j].classList.add("active");
-      } else {
-        productTabTriggers[j].classList.remove("active");
-        productTabContents[j].classList.remove("active");
-      }
-    }
-  });
-  productTabTriggers[1].addEventListener("click", function(e){
-    e.preventDefault();
-    productStickyCard.classList.add("active");
-  });
-  productTabTriggers[3].addEventListener("click", function(e){
-    e.preventDefault();
-    productStickyCard.classList.add("active");
-  });
-  productTabTriggers[0].addEventListener("click", function(e){
-    e.preventDefault();
-    productStickyCard.classList.remove("active");
-  });
-  productTabTriggers[2].addEventListener("click", function(e){
-    e.preventDefault();
-    productStickyCard.classList.remove("active");
-  });
-}
+      for (let j = 0; j < productTabTriggers.length; j++) {
+        let contentAttr = productTabContents[j].getAttribute("data-product-content");
 
-// Табы фотографий товара
-const productImageTriggers = document.querySelectorAll(".pagination-image"),
-      productCurrImage = document.querySelectorAll(".current-image");
-
-for (var i = 0; i < productImageTriggers.length; i++) {
-
-    productImageTriggers[i].addEventListener("click", function(e){
-    e.preventDefault();
-    const activeTabAttr = e.target.getAttribute("data-product-image_trigger");
-
-    for (let j = 0; j < productImageTriggers.length; j++) {
-      let contentAttr = productCurrImage[j].getAttribute("data-product-image_current");
-
-      if (activeTabAttr === contentAttr) {
-        productImageTriggers[j].classList.add("active");
-        productCurrImage[j].classList.add("active"); 
-      } else {
-        productImageTriggers[j].classList.remove("active");
-        productCurrImage[j].classList.remove("active");
-      }
-    }
-  });
-}
-
-// Выбор размера в категории "Все о товаре"
-const sizeTab = document.querySelectorAll('.option-size');
-const sizeContainer = document.querySelector('.option-size__bundle');
-sizeTab.forEach(function(item){
-    item.addEventListener('click', function(e) {
-        const sizeId = this.getAttribute('data-size'),
-              sizeElem = document.querySelector('.option-size__wrapper[data-size="' + sizeId + '"]');
-        for (let i = 0; i < sizeContainer.children.length;i++){
-            sizeContainer.children[i].classList.remove('active');
+        if (activeTabAttr === contentAttr) {
+          productTabTriggers[j].classList.add("active");
+          productTabContents[j].classList.add("active");
+        } else {
+          productTabTriggers[j].classList.remove("active");
+          productTabContents[j].classList.remove("active");
         }
-        sizeElem.classList.add('active');
+      }
     });
-});
-
-// Добавление товара в категорию "Избранное"
-const addToFavorites = document.querySelectorAll('.add-to-favorites__btn'),
-      removeFromFavorites = document.querySelectorAll('.added-to-favorites__btn');
-
-addToFavorites.forEach(function(item){
-    item.addEventListener('click', function(e) {
-        const addFavId = this.getAttribute('data-add-fav'),
-              addFavBtn = document.querySelector('.add-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
-              addFavText = document.querySelector('.add-to-favorites__text[data-add-fav="' + addFavId + '"]'),
-              addedFavBtn = document.querySelector('.added-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
-              addedFavText = document.querySelector('.added-to-favorites__text[data-add-fav="' + addFavId + '"]');
-
-        addFavBtn.classList.add('added');
-        addFavText.classList.add('added');
-        addedFavBtn.classList.add('added');
-        addedFavText.classList.add('added');
+    productTabTriggers[1].addEventListener("click", function(e){
+      
+      productStickyCard.classList.add("active");
     });
-});
-
-removeFromFavorites.forEach(function(item){
-    item.addEventListener('click', function(e) {
-        const addFavId = this.getAttribute('data-add-fav'),
-              addFavBtn = document.querySelector('.add-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
-              addFavText = document.querySelector('.add-to-favorites__text[data-add-fav="' + addFavId + '"]'),
-              addedFavBtn = document.querySelector('.added-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
-              addedFavText = document.querySelector('.added-to-favorites__text[data-add-fav="' + addFavId + '"]');
-
-        addFavBtn.classList.remove('added');
-        addFavText.classList.remove('added');
-        addedFavBtn.classList.remove('added');
-        addedFavText.classList.remove('added');
+    productTabTriggers[3].addEventListener("click", function(e){
+      
+      productStickyCard.classList.add("active");
     });
-});
+    productTabTriggers[0].addEventListener("click", function(e){
+      
+      productStickyCard.classList.remove("active");
+    });
+    productTabTriggers[2].addEventListener("click", function(e){
+      
+      productStickyCard.classList.remove("active");
+    });
+  }
 
-// Скрипт для попапа c таблицей размеров
-document.addEventListener('DOMContentLoaded', function() {
+  // Табы фотографий товара
+  const productImageTriggers = document.querySelectorAll(".pagination-image"),
+        productCurrImage = document.querySelectorAll(".current-image");
+
+  for (var i = 0; i < productImageTriggers.length; i++) {
+
+      productImageTriggers[i].addEventListener("click", function(e){
+      
+      const activeTabAttr = e.target.getAttribute("data-product-image_trigger");
+
+      for (let j = 0; j < productImageTriggers.length; j++) {
+        let contentAttr = productCurrImage[j].getAttribute("data-product-image_current");
+
+        if (activeTabAttr === contentAttr) {
+          productImageTriggers[j].classList.add("active");
+          productCurrImage[j].classList.add("active"); 
+        } else {
+          productImageTriggers[j].classList.remove("active");
+          productCurrImage[j].classList.remove("active");
+        }
+      }
+    });
+  }
+
+  // Выбор размера в категории "Все о товаре"
+  const sizeTab = document.querySelectorAll('.option-size');
+  const sizeContainer = document.querySelector('.option-size__bundle');
+  sizeTab.forEach(function(item){
+      item.addEventListener('click', function(e) {
+          const sizeId = this.getAttribute('data-size'),
+                sizeElem = document.querySelector('.option-size[data-size="' + sizeId + '"]');
+          for (let i = 0; i < sizeContainer.children.length;i++){
+              sizeContainer.children[i].classList.remove('active');
+          }
+          sizeElem.classList.add('active');
+      });
+  });
+
+  // Добавление товара в категорию "Избранное"
+  const addToFavorites = document.querySelectorAll('.add-to-favorites__btn'),
+        removeFromFavorites = document.querySelectorAll('.added-to-favorites__btn');
+
+  addToFavorites.forEach(function(item){
+      item.addEventListener('click', function(e) {
+          const addFavId = this.getAttribute('data-add-fav'),
+                addFavBtn = document.querySelector('.add-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
+                addFavText = document.querySelector('.add-to-favorites__text[data-add-fav="' + addFavId + '"]'),
+                addedFavBtn = document.querySelector('.added-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
+                addedFavText = document.querySelector('.added-to-favorites__text[data-add-fav="' + addFavId + '"]');
+
+          addFavBtn.classList.add('added');
+          addFavText.classList.add('added');
+          addedFavBtn.classList.add('added');
+          addedFavText.classList.add('added');
+      });
+  });
+
+  removeFromFavorites.forEach(function(item){
+      item.addEventListener('click', function(e) {
+          const addFavId = this.getAttribute('data-add-fav'),
+                addFavBtn = document.querySelector('.add-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
+                addFavText = document.querySelector('.add-to-favorites__text[data-add-fav="' + addFavId + '"]'),
+                addedFavBtn = document.querySelector('.added-to-favorites__btn[data-add-fav="' + addFavId + '"]'),
+                addedFavText = document.querySelector('.added-to-favorites__text[data-add-fav="' + addFavId + '"]');
+
+          addFavBtn.classList.remove('added');
+          addFavText.classList.remove('added');
+          addedFavBtn.classList.remove('added');
+          addedFavText.classList.remove('added');
+      });
+  });
+
+  // Скрипт для попапа c таблицей размеров
   const popupTableLinks = document.querySelectorAll('.popup-table-link');
   const bodyOfTable = document.querySelector('body');
   const lockPaddingTable = document.querySelectorAll('.lock-padding');
@@ -128,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
               const popupNameTable = popupLinkTable.getAttribute('href').replace('#', '');
               const currentPopupTable = document.getElementById(popupNameTable);
               popupOpen(currentPopupTable);
-              e.preventDefault();
+              
           });
       }
   }
@@ -138,7 +139,7 @@ document.addEventListener('DOMContentLoaded', function() {
           const el = popupCloseIconCart[index];
           el.addEventListener('click', function (e) {
               popupCloseTable(el.closest('.popup_table'));
-              e.preventDefault();
+              
           });
       }
   }
@@ -206,148 +207,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   
   document.addEventListener('keydown', function (e) {
-      e.preventDefault();
+      
       if (e.which === 27) {
           const popupActiveCart = document.querySelector('.popup_table.open');
           popupCloseTable(popupActiveCart);
       }
   });
-});
 
-
-// Копировать код товара
-function copyCodeText(el) {
-  const $tmp = $("<textarea>");
-
-  $("body").append($tmp);
-  $tmp.val($(el).text()).select();
-  document.execCommand("copy");
-  $tmp.remove();
-
-  $(".ui-elements__product-code__message").addClass('active');
-  $(".message_icon").addClass('active');
-
-  setTimeout(function() { 
-    $(".ui-elements__product-code__message").removeClass('active');
-    $(".message_icon").removeClass('active');
-  }, 3000);
-}
-
-// Свайпер в дополнительном блоке справа в табах "Описание" и "Оставить отзыв"
-let swiperStickyCard = new Swiper('.slider_sticky-card', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-  },
-  simulateTouch: true,
-  touchAngle: 45,
-  slidesPerView: 1,
-  spaceBetween: 17.5,
-  draggable: true,
-  observer: true,
-  observeParents: true,
-  loop: true,
-  autoplay: {
-      delay: 2000,
-  }
-});
-
-// Свайпер в табе товара "Фото"
-let swiperBigGallery = new Swiper('.slider_gallery-big', {
-  simulateTouch: true,
-  touchAngle: 45,
-  centeredSlides: true,
-  slidesPerView: 1.850,
-  spaceBetween: 40,
-  draggable: true,
-  observer: true,
-  observeParents: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    clickable: true,
-  },
-  breakpoints: {
-    992: {
-      slidesPerView: 1.5,
-      spaceBetween: 10,
-    },
-    1200: {
-      slidesPerView: 1.850,
-      spaceBetween: 40,
-    }
-  }
-});
-
-// Свайпер в секции "Вам может понравиться" (320px - 992px)
-let swiperAdditionals = new Swiper('.slider-product_additionals', {
-  simulateTouch: true,
-  touchAngle: 45,
-  draggable: true,
-  slidesPerView: 2.5,
-  loop: true,
-  spaceBetween: 150,
-  freeMode: true,
-  observer: true,
-  observeParents: true,
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-    dragSize: 200,
-  },
-  breakpoints: {
-    280: {
-      slidesPerView: 1,
-      freeMode: false,
-    },
-    320: {
-      slidesPerView: 1.4,
-      freeMode: false,
-      spaceBetween: 100,
-    },
-    375: {
-      slidesPerView: 1.6,
-      freeMode: true,
-      spaceBetween: 100,
-    },
-    425: {
-      slidesPerView: 2,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-    576: {
-      slidesPerView: 2.1,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-    768: {
-      slidesPerView: 2.8,
-      freeMode: true,
-      spaceBetween: 150,
-    },
-  }
-});
-
-// Свайпер в дополнительном блоке справа в табах "Описание" и "Оставить отзыв"
-let swiperCommonMobile = new Swiper('.slider_product-common__mobile', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-  },
-  simulateTouch: true,
-  touchAngle: 45,
-  slidesPerView: 1,
-  spaceBetween: 17.5,
-  draggable: true,
-  observer: true,
-  observeParents: true,
-  loop: true,
-});
-
-// popup-окно "Поделиться"
-document.addEventListener('DOMContentLoaded', function() {
+  // popup-окно "Поделиться"
   const openShare = document.querySelector('.ui-elements__share-icon'),
         modalShare = document.querySelector('.ui-elements__share-popup'),
         closeShare = document.querySelector('.ui-elements__share-close_icon'),
@@ -383,4 +250,20 @@ document.addEventListener('DOMContentLoaded', function() {
   
 });
 
+// Копировать код товара
+function copyCodeText(el) {
+  const $tmp = $("<textarea>");
 
+  $("body").append($tmp);
+  $tmp.val($(el).text()).select();
+  document.execCommand("copy");
+  $tmp.remove();
+
+  $(".ui-elements__product-code__message").addClass('active');
+  $(".message_icon").addClass('active');
+
+  setTimeout(function() { 
+    $(".ui-elements__product-code__message").removeClass('active');
+    $(".message_icon").removeClass('active');
+  }, 3000);
+}
