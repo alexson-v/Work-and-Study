98e76f484f084ade9274e3a23f2e84badf9c2644
Vlad <alexson.personal@gmail.com>
2022-04-15 17:39:52 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "pulse/src"
+            baseDir: "pulse/dist"
         },
     });
 
@@ -22,12 +24,45 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("pulse/src/css"))
+        .pipe(gulp.dest("pulse/dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("pulse/src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("pulse/src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("pulse/src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src("pulse/src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("pulse/dist/"));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("pulse/src/js/**/*.js")
+        .pipe(gulp.dest("pulse/dist/js"));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("pulse/src/fonts/**/*")
+        .pipe(gulp.dest("pulse/dist/fonts"));
+});
+
+gulp.task('icons', function() {
+    return gulp.src("pulse/src/icons/**/*")
+        .pipe(gulp.dest("pulse/dist/icons"));
+});
+
+gulp.task('mailer', function() {
+    return gulp.src("pulse/src/mailer/**/*")
+        .pipe(gulp.dest("pulse/dist/mailer"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("pulse/src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("pulse/dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
