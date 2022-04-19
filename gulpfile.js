b2439547de5a581d8d337782af9ef009daa4017e
Vlad <alexson.personal@gmail.com>
2022-04-19 18:41:08 +0300

    Завершён 82-й урок web-разработки Udemy

--- gulpfile.js
+++ gulpfile.js
@@ -11,58 +11,61 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "pulse/dist"
+            baseDir: "portfolio/dist"
         },
     });
 
-    gulp.watch("pulse/src/*.html").on('change', browserSync.reload);
+    gulp.watch("portfolio/src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("pulse/src/sass/**/*.+(scss|sass)")
+    return gulp.src("portfolio/src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("pulse/dist/css"))
+        .pipe(gulp.dest("portfolio/dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("pulse/src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("pulse/src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("portfolio/src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("portfolio/src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("portfolio/src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("portfolio/src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("portfolio/src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("portfolio/src/img/**/*").on('all', gulp.parallel('images'));
 });
 
 gulp.task('html', function() {
-    return gulp.src("pulse/src/*.html")
+    return gulp.src("portfolio/src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("pulse/dist/"));
+        .pipe(gulp.dest("portfolio/dist/"));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src("pulse/src/js/**/*.js")
-        .pipe(gulp.dest("pulse/dist/js"));
+    return gulp.src("portfolio/src/js/**/*.js")
+        .pipe(gulp.dest("portfolio/dist/js"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
-    return gulp.src("pulse/src/fonts/**/*")
-        .pipe(gulp.dest("pulse/dist/fonts"));
+    return gulp.src("portfolio/src/fonts/**/*")
+        .pipe(gulp.dest("portfolio/dist/fonts"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('icons', function() {
-    return gulp.src("pulse/src/icons/**/*")
-        .pipe(gulp.dest("pulse/dist/icons"));
-});
-
-gulp.task('mailer', function() {
-    return gulp.src("pulse/src/mailer/**/*")
-        .pipe(gulp.dest("pulse/dist/mailer"));
+    return gulp.src("portfolio/src/icons/**/*")
+        .pipe(gulp.dest("portfolio/dist/icons"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('images', function() {
-    return gulp.src("pulse/src/img/**/*")
+    return gulp.src("portfolio/src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("pulse/dist/img"));
+        .pipe(gulp.dest("portfolio/dist/img"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
