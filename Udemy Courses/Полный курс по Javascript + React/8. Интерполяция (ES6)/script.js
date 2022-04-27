f38e9743eb2a0d54b0885ea421a9a78ed7fad86d
Vlad <alexson.personal@gmail.com>
2022-04-27 21:41:34 +0300

    Начат курс Udemy по JavaScript + React

--- Udemy Courses/Полный курс по Javascript + React/8. Интерполяция (ES6)/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`); // чтобы постоянно не плюсовать переменные, ставим косые кавычки
-
-const user = "Ivan"; // применяем на практике
-alert(`Привет, ${user}`);
